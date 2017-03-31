f9f86dc15d49d93628c1abfd938c5144a96371e5
Imteyaz Ahmad <iahmad@shutterfly.com>
2017-03-31 19:56:37 +0530

    Added config for cros origin

--- src/main/java/com/clairvoyant/workshop/controller/ContactController.java
+++ src/main/java/com/clairvoyant/workshop/controller/ContactController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by imteyaz on 31/3/17.
  */
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class ContactController {
 
